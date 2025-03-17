bd0e730cc0c9ad1e1e0f02cd99de763e7726cb24
Joel Varty <joel@agilitycms.com>
2025-03-17 16:23:36 -0400

    dynamic

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,13 +1,13 @@
 
-import {getPageTemplate} from "components/agility-pages"
-import {PageProps, getAgilityPage} from "lib/cms/getAgilityPage"
-import {getAgilityContext} from "lib/cms/useAgilityContext"
-import {Metadata, ResolvingMetadata} from "next"
-import {resolveAgilityMetaData} from "lib/cms-content/resolveAgilityMetaData"
+import { getPageTemplate } from "components/agility-pages"
+import { PageProps, getAgilityPage } from "lib/cms/getAgilityPage"
+import { getAgilityContext } from "lib/cms/useAgilityContext"
+import { Metadata, ResolvingMetadata } from "next"
+import { resolveAgilityMetaData } from "lib/cms-content/resolveAgilityMetaData"
 import NotFound from "./not-found"
 import InlineError from "components/common/InlineError"
-import {SitemapNode} from "lib/types/SitemapNode"
-import {notFound} from "next/navigation"
+import { SitemapNode } from "lib/types/SitemapNode"
+import { notFound } from "next/navigation"
 import agilitySDK from "@agility/content-fetch"
 
 
@@ -20,43 +20,44 @@ export const dynamic = "force-static"
  * Generate the list of pages that we want to generate a build time.
  */
 export async function generateStaticParams() {
+	return []
 	const isDevelopmentMode = process.env.NODE_ENV === "development";
 	const isPreview = isDevelopmentMode;
 	const apiKey = isPreview ? process.env.AGILITY_API_PREVIEW_KEY : process.env.AGILITY_API_FETCH_KEY;
 	const agilityClient = agilitySDK.getApi({
-	  guid: process.env.AGILITY_GUID,
-	  apiKey,
-	  isPreview,
+		guid: process.env.AGILITY_GUID,
+		apiKey,
+		isPreview,
 	});
 	const languageCode = process.env.AGILITY_LOCALES || "en-us";
-  
+
 	agilityClient.config.fetchConfig = {
-	  next: {
-		tags: [`agility-sitemap-flat-${languageCode}`],
-		revalidate: 60,
-	  },
+		next: {
+			tags: [`agility-sitemap-flat-${languageCode}`],
+			revalidate: 60,
+		},
 	};
 
 	const sitemap: { [path: string]: SitemapNode } = await agilityClient.getSitemapFlat({
-	  channelName: process.env.AGILITY_SITEMAP || "website",
-	  languageCode,
+		channelName: process.env.AGILITY_SITEMAP || "website",
+		languageCode,
 	});
-  
+
 	const paths = Object.values(sitemap)
-	  .filter((node, index) => {
-		if (node.redirect !== null || node.isFolder === true || index === 0) return false;
-		if (node.path.startsWith("/api")) return false; // exclude api routes
-		return true;
-	  })
-	  .map((node) => {
-		return {
-		  slug: node.path.split("/").slice(1),
-		};
-	  });
-  
+		.filter((node, index) => {
+			if (node.redirect !== null || node.isFolder === true || index === 0) return false;
+			if (node.path.startsWith("/api")) return false; // exclude api routes
+			return true;
+		})
+		.map((node) => {
+			return {
+				slug: node.path.split("/").slice(1),
+			};
+		});
+
 	console.log("Pre-rendering", paths.length, "static paths.");
 	return paths;
-  }
+}
 
 /**
  * Generate metadata for this page
@@ -64,7 +65,7 @@ export async function generateStaticParams() {
 export async function generateMetadata(
 	props: PageProps,
 	parent: ResolvingMetadata
-  ): Promise<Metadata> {
+): Promise<Metadata> {
 	const { params } = props;  // Remove the 'await' here
 
 	const { locale, sitemap, isDevelopmentMode, isPreview } = await getAgilityContext();
@@ -72,15 +73,15 @@ export async function generateMetadata(
 
 	if (!agilityData.page) return {};
 	return await resolveAgilityMetaData({
-	  agilityData,
-	  locale,
-	  sitemap,
-	  isDevelopmentMode,
-	  isPreview,
-	  parent,
+		agilityData,
+		locale,
+		sitemap,
+		isDevelopmentMode,
+		isPreview,
+		parent,
 	});
-  }
-  export default async function Page({ params }: PageProps) {
+}
+export default async function Page({ params }: PageProps) {
 
 	const agilityData = await getAgilityPage({ params });
 	if (!agilityData.page) notFound();
@@ -88,12 +89,12 @@ export async function generateMetadata(
 	const AgilityPageTemplate = getPageTemplate(agilityData.pageTemplateName || "");
 
 	return (
-	  <div data-agility-page={agilityData.page?.pageID} data-agility-dynamic-content={agilityData.sitemapNode.contentID}>
-		{AgilityPageTemplate ? (
-		  <AgilityPageTemplate {...agilityData} />
-		) : (
-		  <InlineError message={`No template found for page template name: ${agilityData.pageTemplateName}`} />
-		)}
-	  </div>
+		<div data-agility-page={agilityData.page?.pageID} data-agility-dynamic-content={agilityData.sitemapNode.contentID}>
+			{AgilityPageTemplate ? (
+				<AgilityPageTemplate {...agilityData} />
+			) : (
+				<InlineError message={`No template found for page template name: ${agilityData.pageTemplateName}`} />
+			)}
+		</div>
 	);
-  }
+}
